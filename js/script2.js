bfdc2d9d20400b9da331e51a41db9e5acc366eaa
MateusAsh <mateussantosfaria@gmail.com>
2025-04-09 21:11:35 -0300

    Modificados

---
+++ js/script2.js
@@ -0,0 +1,6 @@
+document.write("Estamos executando o script2.js");
+document.write("Esse script está em um arquivo externo!");
+function mudaCor(novaCor){
+    var x = document.getElementById("demo");
+    x.style.color= novaCor;
+}
